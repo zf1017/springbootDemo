dbf1e82723b4bd05bdb27cf819d5855561735264
zhoufei <zf1017@foxmail.com>
2018-11-08 23:29:12 +0800

    springbootdemo

---
+++ src/main/java/com/example/demo/DemoController.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by Administrator on 2018/11/8 0008.
+ */
+@RestController
+public class DemoController {
+    @RequestMapping("/")
+    public String hello(){
+        return "hello-world";
+    }
+}
