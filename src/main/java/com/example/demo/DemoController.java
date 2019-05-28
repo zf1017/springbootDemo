a5ceca7e0a229f8853afba671169e2e7527de54c
舒克 <34359691+zf1017@users.noreply.github.com>
2019-05-28 15:59:28 +0800

    Update DemoController.java

--- src/main/java/com/example/demo/DemoController.java
+++ src/main/java/com/example/demo/DemoController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class DemoController {
     
-    @RequestMapping("/")
+    @RequestMapping("/hello")
     public String hello(){
         return "hello-world";
     }
